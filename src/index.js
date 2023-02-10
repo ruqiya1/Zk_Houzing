f72490e119c9fa7584275d2f4707606666b67a2e
ruqiya1 <kamolazohidova04@gmail.com>
2023-02-10 10:08:23 +0500

    Add home pages

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import Root from "./root";
 import "antd/dist/antd.css";
 import "slick-carousel/slick/slick.css";
+
 import "slick-carousel/slick/slick-theme.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
