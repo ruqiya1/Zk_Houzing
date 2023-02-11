444ce9a893719de1de2868b1f83770ace1ae763d
ruqiya1 <kamolazohidova04@gmail.com>
2023-02-11 19:52:07 +0500

    Add register page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 import Root from "./root";
 import "antd/dist/antd.css";
 import "slick-carousel/slick/slick.css";
-
 import "slick-carousel/slick/slick-theme.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
